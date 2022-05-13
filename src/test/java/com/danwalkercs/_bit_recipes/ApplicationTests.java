43696625584aaa18dfa554cd1f04d100dd1b7900
danla <dan.lawrence.walker@gmail.com>
2022-05-12 19:02:26 -0700

    Initial commit

---
+++ src/test/java/com/danwalkercs/_bit_recipes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danwalkercs._bit_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
