243495ecaf8ca5d09fe2232eaab699af1261fc29
danla <dan.lawrence.walker@gmail.com>
2022-05-13 09:55:16 -0700

    Updated entity based on schema changes

--- src/main/java/com/danwalkercs/eight_bit_recipes/entity/data/Recipe.java
+++ src/main/java/com/danwalkercs/eight_bit_recipes/entity/data/Recipe.java
@@ -7,6 +7,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 
 @Entity
@@ -16,8 +19,9 @@ import javax.persistence.Entity;
 @EqualsAndHashCode(callSuper = true)
 public class Recipe extends AbstractDomainEntity {
 
-    private String creator;
+    private String name;
 
-    // TODO: This will be its own object
-    private String directions;
+    @OneToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "creator", referencedColumnName = "name", nullable = false)
+    private User creator;
 }
